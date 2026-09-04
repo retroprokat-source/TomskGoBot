4cbde0f504427504927d881b37c614895c5aa44a
retroprokat-source <retroprokat@gmail.com>
2026-09-04 23:45:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -390,3 +390,16 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    from threading import Thread
+    Thread(target=lambda: app.run(host="0.0.0.0", port=10000)).start()
+    asyncio.run(main())
