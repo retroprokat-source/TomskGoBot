ca4477fd512ee7ba021c2af66f18ea264543695d
retroprokat-source <retroprokat@gmail.com>
2026-09-04 23:56:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -387,10 +387,6 @@ async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
 from flask import Flask
 
 app = Flask(__name__)
